cab4d9b0551872d3abf77e9f41a350b1d24adbb1
william-tsn <w.tousson@ecole-ipssi.net>
2025-12-16 17:30:40 +0100

    Checkpoint from VS Code for cloud agent session

---
+++ TOUSSON_William_Symfony_Eval/src/Controller/RecipeController.php
@@ -0,0 +1,87 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Recipe;
+use App\Form\RecipeType;
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/recipes')]
+class RecipeController extends AbstractController
+{
+    #[Route('/', name: 'app_recipe_index')]
+    public function index(RecipeRepository $recipeRepository): Response
+    {
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipeRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_recipe_new')]
+    #[IsGranted('ROLE_USER')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $recipe = new Recipe();
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($recipe);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Recette créée avec succès !');
+            return $this->redirectToRoute('app_recipe_index');
+        }
+
+        return $this->render('recipe/new.html.twig', [
+            'recipe' => $recipe,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_recipe_show')]
+    public function show(Recipe $recipe): Response
+    {
+        return $this->render('recipe/show.html.twig', [
+            'recipe' => $recipe,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_recipe_edit')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function edit(Request $request, Recipe $recipe, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Recette modifiée avec succès !');
+            return $this->redirectToRoute('app_recipe_index');
+        }
+
+        return $this->render('recipe/edit.html.twig', [
+            'recipe' => $recipe,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: 'app_recipe_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function delete(Request $request, Recipe $recipe, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$recipe->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($recipe);
+            $entityManager->flush();
+            $this->addFlash('success', 'Recette supprimée avec succès !');
+        }
+
+        return $this->redirectToRoute('app_recipe_index');
+    }
+}
---
+++ TOUSSON_William_Symfony_Eval/src/Form/RecipeType.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Form;
+
+use App\Entity\Recipe;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RecipeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => ['class' => 'form-control', 'rows' => 4]
+            ])
+            ->add('ingredients', TextareaType::class, [
+                'label' => 'Ingrédients',
+                'attr' => ['class' => 'form-control', 'rows' => 6]
+            ])
+            ->add('duration', IntegerType::class, [
+                'label' => 'Durée (en minutes)',
+                'attr' => ['class' => 'form-control']
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Recipe::class,
+        ]);
+    }
+}
