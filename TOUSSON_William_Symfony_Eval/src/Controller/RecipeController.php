9577b723e409f088549c89fd7e3a50d7cba952e3
william-tsn <w.tousson@ecole-ipssi.net>
2025-12-19 20:23:04 +0100

    [UPDATE] Mise a jour et correction de bugs

--- TOUSSON_William_Symfony_Eval/src/Controller/RecipeController.php
+++ TOUSSON_William_Symfony_Eval/src/Controller/RecipeController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Repository\FavoriteRepository;
 use App\Service\RecipeApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,49 +12,37 @@ use Symfony\Component\Routing\Annotation\Route;
 class RecipeController extends AbstractController
 {
     #[Route('', name: 'app_recipes')]
-    public function index(
-        Request $request,
-        RecipeApiService $api,
-        FavoriteRepository $favoriteRepository
-    ): Response {
-        $query = $request->query->get('q');
-
-        if ($query) {
-            $recipes = $api->searchRecipes($query);
-        } else {
-            $recipes = $api->getRecipes();
-        }
-
-        $favoriteIds = [];
-
-        if ($this->getUser()) {
-            $favorites = $favoriteRepository->findBy([
-                'user' => $this->getUser()
-            ]);
-
-            foreach ($favorites as $favorite) {
-                $favoriteIds[] = $favorite->getRecipeId();
-            }
-        }
-
+    public function index(RecipeApiService $api): Response
+    {
         return $this->render('recipe/index.html.twig', [
-            'recipes' => $recipes,
-            'query' => $query,
-            'favoriteIds' => $favoriteIds,
+            'recipes' => $api->getRecipes(),
         ]);
     }
 
     #[Route('/{id}', name: 'app_recipe_show')]
-    public function show(string $id, RecipeApiService $api): Response
-    {
+    public function show(
+        string $id,
+        RecipeApiService $api,
+        FavoriteRepository $favoriteRepository
+    ): Response {
         $recipe = $api->getRecipeById($id);
 
         if (!$recipe) {
             throw $this->createNotFoundException('Recette introuvable');
         }
 
+        $isFavorite = false;
+
+        if ($this->getUser()) {
+            $isFavorite = (bool) $favoriteRepository->findOneBy([
+                'recipeId' => $id,
+                'user' => $this->getUser()
+            ]);
+        }
+
         return $this->render('recipe/show.html.twig', [
-            'recipe' => $recipe,
+            'recipe'     => $recipe,
+            'isFavorite' => $isFavorite,
         ]);
     }
 }
